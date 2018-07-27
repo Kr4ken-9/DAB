ef6baa5c14c38d8b2652587617faf5c6ba52258b
Kr4ken <kr4kenkt@gmail.com>
2018-07-26 22:14:40 -0500

    Revert "hot mess of stuff I don't remember what I was doing"
    This reverts commit 78cab4596c6daa81ebeb00a77211f9915e1b0e0e.

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ def check_config(path):
     if not os.path.isfile(path):
         return False
 
-    _config = load_config(path)
-    if _config['token'] == 'x':
+    config = yaml.load(path)
+    if config['token'] == 'x':
         return False
 
     return True
@@ -21,21 +21,3 @@ def load_config(path):
 def save_config(path, config):
     with open(path, 'w') as file:
         file.write(yaml.dump(config))
-
-def assemble_config():
-    token = input('Enter your token: ')
-    repfarming = input('Would you like to enable rep farming? y/n ')
-
-    repconfig = load_config('repconfig.yaml')
-    config = load_config('config.yaml')
-
-    if 'y' in repfarming:
-        recipient = input('Enter the id of the user you would like to farm rep for: ')
-
-        repconfig['recipients'] = [recipient]
-    else:
-        repconfig['recipients'] = False
-
-    config['token'] = token
-    save_config('config.yaml', config)
-    save_config('repconfig.yaml', repconfig)
