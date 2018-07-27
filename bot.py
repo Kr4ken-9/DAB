5de5980e3224decf17a6c7034c199f02a6ad94c4
Kr4ken <kr4kenkt@gmail.com>
2018-07-27 07:43:50 -0500

    Documentation and command module

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 import asyncio
 import random
 import config
+import commands
 
 client = discord.Client()
 rand = random.SystemRandom()
@@ -16,56 +17,46 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author.id != client.user.id and message.author.id not in _config['owners']:
-        return
+    """Check all incoming messages to see if they are commands, handle them if they are
 
-    if message.author.id != client.user.id and message.content.startswith('|'):
-        await client.send_message(message.channel, message.content[1:])
-        return
-
-    if message.content.startswith('_add'):
-        _config['channels'].append(message.channel.id)
-
-    if message.content.startswith('_remove'):
-        _config['channels'].remove(message.channel.id)
-
-    if message.content.startswith('_save'):
-        config.save_config('config.yaml', _config)
-        config.save_config('repcofnig.yaml', repconfig)
+    :param message: Incoming message
+    """
+    commands.handle_command(client, message, [_config, repconfig])
 
 
 async def farm():
+    """Send messages according to yaml config specifying frequency, channel, etc"""
     await client.wait_until_ready()
 
     while not client.is_closed:
-        if len(_config['channels']) == 0:
+        if len(_config['channels']) == 0: # Don't proceed if no channels are configured
             if type(_config['delay']) is list:
-                minmax = _config['delay']
-                await asyncio.sleep(rand.randint(minmax[0], minmax[1]))
+                minmax = _config['delay'] # If the delay config option is a list, treat it as parameters
+                await asyncio.sleep(rand.randint(minmax[0], minmax[1])) # First parameter is the minimum, second is the maximum
                 continue
 
-            await asyncio.sleep(_config['delay'])
+            await asyncio.sleep(_config['delay']) # Wait for the configured amount of time before continuing
             continue
 
         channels = _config['channels']
-        if _config['randomchannels']:
+        if _config['randomchannels']: # If configured, shuffle the channels to randomize the order we farm them
             rand.shuffle(channels)
 
         for schannel in channels:
-            channel = discord.Object(id=schannel)
-            message = await client.send_message(channel, rand.choice(_config['messages']))
+            channel = discord.Object(id=schannel) # Create a channel object of the configured channel id
+            message = await client.send_message(channel, rand.choice(_config['messages'])) # Send a random message in the configured channel
 
-            if _config['silent']:
-                if type(_config['silent']) is list:
+            if _config['silent']: # If configured, delete messasges after they are sent
+                if type(_config['silent']) is list: # If configured, treat silent config option as parameters
                     minmax = _config['silent']
-                    await asyncio.sleep(rand.randint(minmax[0], minmax[1]))
+                    await asyncio.sleep(rand.randint(minmax[0], minmax[1])) # First parameter is the minimum, second is the maximum
                 else:
-                    if _config['silent'] > 0:
+                    if _config['silent'] > 0: # If a static delay is configured, use it
                         await asyncio.sleep(_config['silent'])
 
-                await client.delete_message(message)
+                await client.delete_message(message) # Delete the message after any configured delays
 
-        if type(_config['delay']) is list:
+        if type(_config['delay']) is list: # Delay the farming loop by the configured amount
             minmax = _config['delay']
             await asyncio.sleep(rand.randint(minmax[0], minmax[1]))
             continue
@@ -73,18 +64,19 @@ async def farm():
         await asyncio.sleep(_config['delay'])
 
 async def rep():
+    """Send messages to add rep to the configured person at a configured interval"""
     await client.wait_until_ready()
     
-    if not repconfig['recipients']:
+    if not repconfig['recipients']: # If disabled in configuration, don't proceed
         return
     
     users = user_generator(repconfig['recipients'])
     
     while not client.is_closed:
         channel = discord.Object(id=repconfig['channel'])
-        await client.send_message(channel, f"t!rep <@{next(users)}>")
+        await client.send_message(channel, f"t!rep <@{next(users)}>") # Send a message adding rep to the configured person
 
-        if type(repconfig['delay']) is list:
+        if type(repconfig['delay']) is list: # Delay the loop if configured
             minmax = repconfig['delay']
             await asyncio.sleep(rand.randint(minmax[0], minmax[1]))
         else:
@@ -92,7 +84,7 @@ async def rep():
 
 def user_generator(users):
     random.shuffle(users)
-    c = 0;
+    c = 0
     while True:
         yield users[c]
         c += 1
--- config.py
+++ config.py
@@ -3,6 +3,11 @@ import yaml
 
 
 def check_config(path):
+    """Check if config exists and is usable
+
+    :param path: Path to config
+    :return: Returns a bool representing whether the path exists and is a usable config
+    """
     if not os.path.isfile(path):
         return False
 
@@ -14,10 +19,19 @@ def check_config(path):
 
 
 def load_config(path):
+    """Loads path as a yaml config
+
+    :param path: Path to yaml config
+    """
     with open(path, 'r') as file:
         return yaml.load(file)
 
 
 def save_config(path, config):
+    """Saves yaml config to path
+
+    :param path: Path to save yaml config
+    :param config: Yaml config to save to path
+    """
     with open(path, 'w') as file:
         file.write(yaml.dump(config))
